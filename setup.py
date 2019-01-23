73814a6668db04dbd726e19e17a579be8c0779f3
tatHi <hiraoka.tatsuya.hq7@is.naist.jp>
2019-01-23 13:40:08 +0900

    tmp

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='common',
+    version="0.0.1",
+    description="my common package",
+    long_description="my commoooooon package",
+    url='hoge',
+    author='tathi',
+    author_email='fuga',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    keywords='common',
+    install_requires=['mecab-python3']
+)
