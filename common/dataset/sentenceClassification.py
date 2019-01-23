44b17e5ff0ea4b190fd47fe58aca4092e2f81504
tatHi <hiraoka.tatsuya.hq7@is.naist.jp>
2019-01-23 14:13:15 +0900

    add sentence dataset

--- common/dataset/sentenceClassification.py
+++ common/dataset/sentenceClassification.py
@@ -1,1 +1,6 @@
-pass
+import dataset
+class Dataset4SentenceClassification(dataset.Dataset):
+    def __init__(self, textPathes, labelPathes, split=True):
+        super().__init__(textPathes,split)
+        self.labels = [[line.strip() for line in open(path)] for path in labelPathes]
+
